4d6090f1b31cf32e74267169e6a2b9f5881bcbf3
rostilucena <rosti.lucena@gmail.com>
2024-12-24 15:16:30 -0400

    arriba

--- marinos/seaSpace/models.py
+++ marinos/seaSpace/models.py
@@ -1,24 +1,3 @@
 from django.db import models
 
 # Create your models here.
-''' class  Marinos(models.Model):
-    cedula = models.CharField(max_length =12, verbose_name="Cedula", null=False,blank=False, primary_key=True)
-    nombres = models.CharField(max_length =40, verbose_name="Nombres")
-    apellidos = models.CharField(max_length =40, verbose_name="Apellidos")
-    fechaNacimiento= models.DateField(verbose_name="Fecha de Nacimiento")
-    fechaControl= models.DateField(verbose_name="Fecha de Nacimiento")
-
-class certificadoMarino(models.Model):
-    registro= models.IntegerField()
-    cedulaMarino = models.CharField(verbose_name="Cedula Marino", null=False,blank=False, max_length=12)
-    idDocumento = models.IntegerField(verbose_name="Tipo de Documento")
-    fechaExpedicion = models.DateField(verbose_name="Fecha de Expedición Documento")
-    fechavencimiento= models.DateField(verbose_name="Fecha de vencimiento Documento")
-    fechaControl= models.DateField()
-
-class catalogoDocumetosMarinos(models.Model):
-    idDocumento= models.IntegerField()
-    nombreDocumento= models.CharField(max_length= 60,verbose_name="Nombre del Documento")
-    codigoGeneralDocumento = models.CharField(max_length=50, verbose_name="Código o Nobre Internacional del Documento")
-    
-'''
--- marinos/seaSpace/views.py
+++ marinos/seaSpace/views.py
@@ -1,6 +1,3 @@
 from django.shortcuts import render
 
-#from django.http import HttpResponse
 # Create your views here.
-def IndexView(request):
-    return render(request,"index.html") 
